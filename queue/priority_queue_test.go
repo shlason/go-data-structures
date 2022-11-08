80dfa313d101d33567e27bbf7603ef704b0a5871
Jason <nocvi111@gmail.com>
2022-11-08 13:38:13 +0800

    feat: priority queue structure and testing

---
+++ queue/priority_queue_test.go
@@ -0,0 +1,56 @@
+package queue
+
+import (
+	"testing"
+)
+
+func TestPriorityQueue(t *testing.T) {
+	pq := NewPriority()
+
+	if pq.Size() != 0 {
+		t.Errorf("Expected priority queue size is 0, but got %v", pq.Size())
+	}
+
+	if !pq.IsEmpty() {
+		t.Errorf("Expected priority queue is empty")
+	}
+
+	datas := []string{"1", "22", "55555", "333"}
+
+	for _, data := range datas {
+		pq.Enqueue(data, len(data))
+	}
+
+	if pq.Size() != len(datas) {
+		t.Errorf("Expected priority queue size is %v, but got %v", len(datas), pq.Size())
+	}
+
+	if pq.IsEmpty() {
+		t.Errorf("Expected priority queue is not empty")
+	}
+
+	d1 := pq.Dequeue()
+
+	if d1 != datas[0] {
+		t.Errorf("Expected dequeue element is %v, but got %v", datas[0], d1)
+	}
+
+	pq.Dequeue()
+	d2 := pq.Dequeue()
+
+	if d2 != datas[3] {
+		t.Errorf("Expected dequeue element is %v, but got %v", datas[3], d2)
+	}
+
+	if pq.Front() != datas[2] {
+		t.Errorf("Expected front element is %v, but got %v", datas[2], pq.Front())
+	}
+
+	if pq.Size() != 1 {
+		t.Errorf("Expected priority queue size is 1, but got %v", pq.Size())
+	}
+
+	if pq.IsEmpty() {
+		t.Errorf("Expected priority queue is not empty")
+	}
+}
