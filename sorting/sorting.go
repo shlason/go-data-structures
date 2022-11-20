0a8e0ced376f54880b9b7204b86f13bda0257119
Jason <nocvi111@gmail.com>
2022-11-20 17:58:28 +0800

    feat: quick sort v2

--- sorting/sorting.go
+++ sorting/sorting.go
@@ -147,6 +147,36 @@ func quickSortRec(l []int) {
 	quickSortRec(l[i+1:])
 }
 
+func (l *list) QuickSortV2() {
+	quickSortRecV2(l.items)
+}
+
+func quickSortRecV2(list []int) []int {
+	if len(list) < 2 {
+		return list
+	}
+
+	left, right := 0, len(list)-1
+
+	pivot := 0
+
+	list[pivot], list[right] = list[right], list[pivot]
+
+	for i, _ := range list {
+		if list[i] < list[right] {
+			list[left], list[i] = list[i], list[left]
+			left++
+		}
+	}
+
+	list[left], list[right] = list[right], list[left]
+
+	quickSortRecV2(list[:left])
+	quickSortRecV2(list[left+1:])
+
+	return list
+}
+
 func (l *list) SequentialSearch(target int) int {
 	for _, item := range l.items {
 		if item == target {
